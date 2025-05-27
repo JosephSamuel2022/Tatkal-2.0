dfa3ce90184840adc808f1a20b0b5763f437b535
Joseph Samuel M <101868236+JosephSamuel2022@users.noreply.github.com>
2025-05-27 22:03:30 +0530

    Added Comments for Payment APIs

--- src/main/java/com.tatkal/service/Payment/StripeService.java
+++ src/main/java/com.tatkal/service/Payment/StripeService.java
@@ -16,6 +16,13 @@ public class StripeService {
     private String successUrl;
     @Value("${stripe.cancel-url}")
     private String cancelUrl;
+
+    /**
+     * This method is called when the user clicks the "Make Payment" button.
+     * It creates a Stripe Checkout session using the payment details provided.
+     * The session URL is used to redirect the user to the Stripe payment page.
+     * We use this session ID to poll the status in a separate function.
+     */
     public StripeResponse doPayment(PaymentDAO paymentDAO){
         Stripe.apiKey = secretKey;
         SessionCreateParams.LineItem.PriceData.ProductData productData = SessionCreateParams.LineItem.PriceData.ProductData.builder()
@@ -42,6 +49,12 @@ public class StripeService {
                 .build();
     }
 
+    /**
+     * This method polls the Stripe API using the session ID to check the payment status.
+     * It is called repeatedly (with some delay) for up to 5 minutes after payment session creation.
+     * If the payment status is "paid", we proceed with the booking.
+     * Otherwise, after timeout or failure, the payment is considered unsuccessful.
+     */
     public String getPaymentStatus(String sessionId) {
         Stripe.apiKey = secretKey;
         try {
